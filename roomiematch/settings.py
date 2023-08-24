04b567e778da256ed30a28a785a951c5b8d808be
Signs Ogundare <oreoluwasigns@gmail.com>
2023-08-24 11:10:33 +0100

    Refactor: roomiematch/settings.py for Azure deployment

--- roomiematch/settings.py
+++ roomiematch/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS').split(' ')
 
 
 # Application definition
